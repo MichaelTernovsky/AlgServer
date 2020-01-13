4c396322666622320695c5f0ba6985245b62bc9d
ofirbe <agosl2007@gmail.com>
2020-01-13 23:27:39 +0200

    write command update

--- SockStreams.h
+++ SockStreams.h
@@ -5,11 +5,22 @@
 #ifndef EX4__SOCKSTREAMS_H_
 #define EX4__SOCKSTREAMS_H_
 #include "AlgServer.h"
+#include <string.h>
+#include "sys/socket.h"
+#include <unistd.h>
 
 bool SocketOutputStream::writeToStream(string str) {
-
+  int isSend=0;
+  const char* msg = str.c_str();
+  isSend=write(this->socket,msg,strlen(msg));
+  if(isSend<0){
+    cout<<"Error during write a massage! "<<endl;
+    return false;
+  }
+  return true;
 }
-
+//distractor///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Need to Update.
 string SocketInputStream::readFromStream() {
 
 }
