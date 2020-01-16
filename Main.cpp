b1ec204b41d8b40a6047c23109a9339b50220d46
michaeltern <michaelter5656@gmail.com>
2020-01-16 21:49:00 +0200

    cache manager is done + reversing world

--- Main.cpp
+++ Main.cpp
@@ -9,18 +9,27 @@ using namespace server_side;
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  int port = atoi(argv[1]);
-  int capacity = 5;
+//  int port = atoi(argv[1]);
+//  int capacity = 5;
+//  CacheManager<string, string> *ch = new FileCacheManager<string, string>(capacity);
+//
+//  Searcher<string, string> *s = new BFS<string, string>();
+//  Solver<string, string> *mySolver = new OA<string, string, string>(s);
+//  ClientHandler *clientHand = new MyClientHandler<string, string>(mySolver, ch);
+//
+//  MySerialServer *mySerialServ = new MySerialServer();
+//  mySerialServ->open(port, clientHand);
 
+  string str = "8,2,8\n"
+               "4,12,2\n"
+               "12, 1, 9\n"
+               "0,0\n"
+               "36,36\n"
+               "end";
 
-  CacheManager<string, string> *ch = new FileCacheManager<string, string>(capacity);
-
-  Searcher<string,string> *s = new BFS<string,string>();
-  Solver<string, string> *mySolver = new OA<string, string, string>(s);
-  ClientHandler *clientHand = new MyClientHandler<string, string>(mySolver, ch);
-
-  MySerialServer *mySerialServ = new MySerialServer();
-  mySerialServ->open(port, clientHand);
+  MatrixProblem<string> *m = new MatrixProblem<string>(str);
+  cout << m->getNumRows() << endl;
+  cout << m->getNumCols() << endl;
 
   return 0;
 }
--- MatrixProblem.h
+++ MatrixProblem.h
@@ -7,16 +7,85 @@
 
 #include "Searchable.h"
 #include <iostream>
+#include <cstring>
+#include <vector>
+#include <sstream>
 using namespace std;
 template<typename T>
 class MatrixProblem : public Searchable<T> {
-
  private:
-  string str;
+  string matrixStr;
+  int colsNum;
+  int rowNum;
+  vector<vector<int> > matrixVct;
 
  public:
+  /**
+   * CTOR
+   * @param str
+   */
   MatrixProblem(string str) {
-    this->str = str;
+    this->matrixStr = str;
+    this->rowNum = this->getNumRows();
+    this->colsNum = this->getNumCols();
+    this->creatingMatrixVector();
+  }
+
+  /**
+   * getNumRows - returning the number of rows in the matrix.
+   * @return int - the number of rows.
+   */
+  int getNumRows() {
+    string tmp = this->matrixStr;
+    int nCounter = 0;
+    int tmpLen = tmp.length();
+
+    for (int k = 0; k < tmpLen; k++) {
+      if (tmp[k] == '\n')
+        nCounter++;
+    }
+    return nCounter - 2;
+  }
+
+  /**
+ * getNumCols - returning the number of cols in the matrix.
+ * @return int - the number of cols.
+ */
+  int getNumCols() {
+    string tmp = this->matrixStr;
+    int i = 0;
+    int countRows = 0;
+
+    while (tmp[i] != '\n') {
+      if (tmp[i] != ',')
+        countRows++;
+      i++;
+    }
+    return countRows;
+  }
+
+  /**
+   * creatingMatrixVector - the function creates the matrix from the str field.
+   */
+  void creatingMatrixVector() {
+    string tmp = this->matrixStr;
+    vector<int> vect;
+    stringstream s(tmp);
+
+    for (int j = 0; j < this->rowNum; j++) {
+      // creating the vector - each col of the matrix
+      for (int i; s >> i;) {
+        vect.push_back(i);
+        if (s.peek() == ',')
+          s.ignore();
+        if (s.peek() == '\n')
+          break;
+      }
+
+      // filling the rows of the matrix
+      this->matrixVct.push_back(vect);
+      vect.clear();
+    }
   }
 
   State<T> getInitialState() {
@@ -24,11 +93,9 @@ class MatrixProblem : public Searchable<T> {
   };
   bool isGoalState(State<T> *st) {
     cout << "goalState@#R%@#%@" << endl;
-
   };
   State<T> *getAllPossibleStates(State<T> *st) {
     cout << "getAllPossibleStates@#R%@#%@" << endl;
-
   };
 };
 
