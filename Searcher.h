4a9267ca6e58306ca6a01c01039d23cf9a061ca7
michaeltern <michaelter5656@gmail.com>
2020-01-17 17:29:02 +0200

    cache manager is done + reversing world

--- Searcher.h
+++ Searcher.h
@@ -11,7 +11,6 @@ template<typename T, typename S>
 class Searcher {
  public:
   virtual S search(Searchable<T> *searchObj) {};
-  virtual string whereYouComeFrom() {};
 };
 
 #endif //EX4__SEARCHER_H_
